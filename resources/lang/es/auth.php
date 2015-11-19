4e75436bf84386bb614a9bb3a0811ab0437e996a
Aleks Flores <aleksgnu@gmail.com>
2015-11-19 09:40:58 -0600

    Repo Laravel 5.1 Registro y Login con Bootstrap3 =D

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'errors_title'		=>	'Por favor corrige los siguientes errores',
+    'login_title'       =>  'Inicio de sesión',
+    'login_button'      =>  'Inicia sesión',
+    'register_title'    =>  'Registro',
+    'register_button'   =>  'Registrate',
+    'remember'          =>  '¡Recuérdame!',
+    'forgot_link'       =>  '¿Olvidaste tú contraseña?'
+);
